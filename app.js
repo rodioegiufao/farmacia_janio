9e29dce466dfe7c71459184d985120b47c339441
rodioegiufao <126606600+rodioegiufao@users.noreply.github.com>
2026-01-06 17:19:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -786,7 +786,7 @@ const FARMACIA_MODEL_TRANSFORMS = {
 const POLICLINICA_MODEL_TRANSFORMS = {
     IFC_EST_PP: { position: [-80, 0.4, 50] },
     IFC_ELE_T: { position: [-78, 0, 40] },
-    IFC_PLU: { position: [-78, 0, 40.048] },
+    IFC_PLU: { position: [-78, 0, 40] },
 };
 
 const DEFAULT_MODEL_TRANSFORMS = {
@@ -2288,6 +2288,7 @@ viewer.scene.canvas.canvas.addEventListener('contextmenu', (event) => {
     canvasElement.addEventListener('touchend', endTouch, { passive: false });
     canvasElement.addEventListener('touchcancel', clearTouch, { passive: true });
 })();
+
 
 
 

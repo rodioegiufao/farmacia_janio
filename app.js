26a61439046a88e3757e5c6acdf507ef42be471c
rodioegiufao <126606600+rodioegiufao@users.noreply.github.com>
2026-01-06 11:43:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -740,7 +740,7 @@ const IPER_MODEL_TRANSFORMS = {
     IFC_EST_SUB: { position: [27.66, 0, -22.35], rotation: [0, -84, 0] },
     IFC_EST_CT: { position: [-14.4, 0, -16.27], rotation: [0, 90, 0] },
     IFC_EST_MR: { position: [35.25, 0.4, 20.2], rotation: [0, 90, 0] },
-    IFC_EST_MRC: { position: [-22.95, -0.65, 28.88] },
+    IFC_EST_MRC: { position: [-22.95, 0.4, 28.88] },
     IFC_EMT_ESC: { position: [0.14, 0.35, -0.15], rotation: [0, 90, 0] },
     IFC_EMT_COB: { position: [0.14, 0, -0.15], rotation: [0, 90, 0] },
 };
@@ -2217,6 +2217,7 @@ viewer.scene.canvas.canvas.addEventListener('contextmenu', (event) => {
     canvasElement.addEventListener('touchend', endTouch, { passive: false });
     canvasElement.addEventListener('touchcancel', clearTouch, { passive: true });
 })();
+
 
 
 

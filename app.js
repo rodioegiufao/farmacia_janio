cdd921e9c54ad32361f37cd45452c9d869317f04
rodioegiufao <126606600+rodioegiufao@users.noreply.github.com>
2026-01-16 09:39:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -767,25 +767,25 @@ const defaultModels = [
 
 const IPER_MODEL_TRANSFORMS = {
     IFC_EST: { position: [-8.789, 0.4, 22.48] },
-    IFC_ILUX: { position: [-14, 0, 0] },
     IFC_SPDA: { position: [0.15, 0, 13.9], rotation: [0, 90, 0] },
     IFC_LOG_TEF: { position: [0.16, 0, -0.19], rotation: [0, 90, 0] },
     IFC_ELE: { position: [0.16, 0, -0.19] },
     IFC_SAN: { position: [0.2, 0, 13.9], rotation: [0, 90, 0] },
-    IFC_SUB: { position: [2.3, 0, 2.54], rotation: [0, 95.863, 0] },
+    IFC_SUB: { position: [2.7, -0.25, 2.68], rotation: [0, 96, 0] },
     IFC_INC: { position: [0.15, 0, -0.15], rotation: [0, 90, 0] },
     IFC_HID: { position: [0.2, 0, 13.9], rotation: [0, 90, 0] },
     IFC_PLU: { position: [0.2, 0, 13.9], rotation: [0, 90, 0] },
-    IFC_FOT: { position: [0, 0, 14], rotation: [0, 90, 0] },
-    IFC_CLI: { position: [0.16, 0, 13.9], rotation: [0, 90, 0] },
+    IFC_FOT: { position: [-14, 0, 0]},
+    IFC_CLI: { position: [0.16, 0, 13.9], rotation: [0, 90, 0]  },
     IFC_ALI: { position: [0.15, 0, -0.17] },
-    IFC_EST_SQD: { position: [18.1, 0, -13.92] },
-    IFC_EST_SUB: { position: [27.66, 0, -22.35], rotation: [0, -84, 0] },
-    IFC_EST_CT: { position: [-14.4, 0, -16.27], rotation: [0, 90, 0] },
-    IFC_EST_MR: { position: [35.25, 0.4, 20.2], rotation: [0, 90, 0] },
+    IFC_EST_SQD: { position: [18.1, -0.55, -13.92] },
+    IFC_EST_SUB: { position: [27.66, -0.55, -22.35], rotation: [0, -84, 0] },
+    IFC_EST_CT: { position: [-14.4, 0, -16.27], rotation: [0, 90, 0]  },
+    IFC_EST_MR: { position: [35.25, 0.4, 20.2], rotation: [0, 90, 0]  },
     IFC_EST_MRC: { position: [-22.95, 0.4, 28.88] },
-    IFC_EMT_ESC: { position: [0.14, 0.35, -0.15], rotation: [0, 90, 0] },
-    IFC_EMT_COB: { position: [0.14, 0, -0.15], rotation: [0, 90, 0] },
+    IFC_EMT_ESC: { position: [0.14, 0.35, -0.15], rotation: [0, 90, 0]  },
+    IFC_EMT_COB: { position: [0.14, 0, -0.15], rotation: [0, 90, 0]  },
+    IFC_ILUX: { position: [-14, 0, 0]},
 };
 
 const FARMACIA_MODEL_TRANSFORMS = {
@@ -2316,6 +2316,7 @@ viewer.scene.canvas.canvas.addEventListener('contextmenu', (event) => {
     canvasElement.addEventListener('touchend', endTouch, { passive: false });
     canvasElement.addEventListener('touchcancel', clearTouch, { passive: true });
 })();
+
 
 
 

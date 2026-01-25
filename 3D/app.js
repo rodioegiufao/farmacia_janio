cc063eb859944f6fb946980dd9c1c38fe97addd6
rodioegiufao <126606600+rodioegiufao@users.noreply.github.com>
2026-01-25 21:20:41 +0000

    Tentando melhorar a interface

--- 3D/app.js
+++ 3D/app.js
@@ -44,7 +44,7 @@ const viewer = new Viewer({
     transparent: false, 
     saoEnabled: true,
     edgesEnabled: true,
-    backgroundColor: [0.11, 0.13, 0.15],
+    backgroundColor: [0.05, 0.06, 0.12],
     
     // CONFIGURAÇÃO DE LOCALIZAÇÃO (NavCube em Português)
     localeService: new LocaleService({

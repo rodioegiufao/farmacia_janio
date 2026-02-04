4a9ecf16f288690aa60bdda4eeebf416036aa8af
rodioegiufao <126606600+rodioegiufao@users.noreply.github.com>
2026-02-04 22:17:32 +0000

    Tentando melhorar as propriedades IFC

--- 3D/app.js
+++ 3D/app.js
@@ -1050,6 +1050,7 @@ const POLICLINICA_MODEL_TRANSFORMS = {
 
 const CANAA_MODEL_TRANSFORMS = {
     IFC_EST_PP: { position: [62.3, 0.15, -36.8995], rotation: [0, 180, 0] },
+    IFC_ELE: { position: [0, -0.05, 0] },
 };
 
 const DEFAULT_MODEL_TRANSFORMS = {

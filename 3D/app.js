8df1626ca77c901c4497064e5a09fb5b3f50872d
rodioegiufao <126606600+rodioegiufao@users.noreply.github.com>
2026-02-10 22:16:27 +0000

    Tentando ATUALIZAR

--- 3D/app.js
+++ 3D/app.js
@@ -1270,7 +1270,7 @@ const POLICLINICA_MODEL_TRANSFORMS = {
 const CANAA_MODEL_TRANSFORMS = {
     IFC_EST_PP: { position: [48.212, 0.37, -36.8995], rotation: [0, 180, 0]},
     IFC_SAN: { position: [0, 0, 0], rotation: [0, 180, 0] },
-    IFC_HID: { position: [0, 0, 0], rotation: [0, 180, 0] },
+    //IFC_HID: { position: [0, 0, 0], rotation: [0, 180, 0] },
 };
 
 const DEFAULT_MODEL_TRANSFORMS = {

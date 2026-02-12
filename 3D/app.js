c55c1eae36a14f8c48a9ede1e797435772c0caf3
rodioegiufao <126606600+rodioegiufao@users.noreply.github.com>
2026-02-12 14:51:01 -0400

    Update app.js

--- 3D/app.js
+++ 3D/app.js
@@ -1313,7 +1313,7 @@ const CANAA_MODELS = [
     { id: "IFC_SAN", src: "/3D/esc_canaa/modelo-12.xkt" },
     { id: "IFC_EXA", src: "/3D/esc_canaa/modelo-13.xkt" },
     { id: "IFC_ARQ", src: "/3D/esc_canaa/modelo-14.xkt" },
-    { id: "IFC_ARQ", src: "/3D/esc_canaa/modelo-15.xkt" },
+    { id: "IFC_PLU", src: "/3D/esc_canaa/modelo-15.xkt" },
   ];
 
 const defaultModels = [
@@ -3712,5 +3712,6 @@ viewer.scene.canvas.canvas.addEventListener('contextmenu', (event) => {
     canvasElement.addEventListener('touchcancel', clearTouch, { passive: true });
 
 })();
+
 
 

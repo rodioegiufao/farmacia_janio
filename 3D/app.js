659ad4f5045e90ea549cab719e6a54c5c201eeac
rodioegiufao <126606600+rodioegiufao@users.noreply.github.com>
2026-03-05 21:59:12 +0000

    Atualizando IFC

--- 3D/app.js
+++ 3D/app.js
@@ -1572,7 +1572,7 @@ const CANAA_MODEL_TRANSFORMS = {
     IFC_EST_PP: { position: [48.212, 0.385, -36.8995], rotation: [0, 180, 0]},
     IFC_SAN: { position: [14.062,-0.1, 0], rotation: [0, 180, 0] },
     //IFC_PLU: { position: [0, 0, 0], rotation: [0, 180, 0] },
-    IFC_ARQ: { position: [48.24, 0.02, -36.8695], rotation: [0, 180, 0]},
+    IFC_ARQ: { position: [48.25, 0, -36.8695]},
     IFC_EST_EMT: { position: [48.212, 0.075, -36.8995], rotation: [0, 180, 0]},
 };
 

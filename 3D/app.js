d6c98bd8f57456c65601f043cde308035b86f04e
rodioegiufao <126606600+rodioegiufao@users.noreply.github.com>
2026-03-19 19:49:24 +0000

    Resolve merge conflicts

--- 3D/app.js
+++ 3D/app.js
@@ -2061,7 +2061,7 @@ const IPER_MODEL_TRANSFORMS = {
     IFC_EST_MRC: { position: [-23, 0.35, 28.88] },
     IFC_EMT_ESC: { position: [0.14, 0.35, -0.15], rotation: [0, 90, 0]  },
     IFC_EMT_COB: { position: [0.14, 0, -0.15], rotation: [0, 90, 0]  },
-    IFC_ILUX: { position: [-14, 0, 0]},
+    //IFC_ILUX: { position: [-14, 0, 0]},
     IFC_ARQ: { position: [0.16, 0, -0.19], rotation: [0, 90, 0]  },
     IFC_SDAI: { position: [0.16, 0, -0.19] },
 };
